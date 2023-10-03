22a1fed42ce7e93b2a86afe71cc85c605fd2a9bc
havya123 <haivo006@gmail.com>
2023-10-03 17:52:43 +0700

    Update size_config.dart

--- lib/config/size_config.dart
+++ lib/config/size_config.dart
@@ -10,6 +10,10 @@ class MyWidget extends StatefulWidget {
 class _MyWidgetState extends State<MyWidget> {
   @override
   Widget build(BuildContext context) {
-    return SafeArea(child: Container(),);
+    return SafeArea(
+      child: Container(
+        child: const Text(""),
+      ),
+    );
   }
 }
