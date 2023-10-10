983f88452dc21e3b3fc36807d68e26ed12c39502
havya123 <haivo006@gmail.com>
2023-10-10 13:34:00 +0700

    size

---
+++ lib/config/size_config.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/material.dart';
+
+double getHeight(context, {height = 0.04}) =>
+    MediaQuery.sizeOf(context).height * height;
+
+double getWidth(context, {width = 0.04}) =>
+    MediaQuery.sizeOf(context).width * width;
+
+SizedBox spaceHeight(context, {height = 0.04}) =>
+    SizedBox(height: MediaQuery.sizeOf(context).height * height);
+
+SizedBox spaceWidth(context, {width = 0.04}) =>
+    SizedBox(width: MediaQuery.sizeOf(context).width * width);
