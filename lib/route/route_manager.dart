588b0715bff5c9b4eb86688b2616ad8cdec5e274
havya123 <haivo006@gmail.com>
2023-10-22 11:38:39 +0700

    commit

--- lib/route/route_manager.dart
+++ lib/route/route_manager.dart
@@ -1,12 +1,17 @@
+import 'package:app_xem_tro/screen/admin_screen/admin_account_screen.dart';
+import 'package:app_xem_tro/screen/admin_screen/admin_approve_screen.dart';
+import 'package:app_xem_tro/screen/admin_screen/admin_booking_screen.dart';
+import 'package:app_xem_tro/screen/admin_screen/admin_screen.dart';
 import 'package:app_xem_tro/screen/chat_screen/chat_screen.dart';
 import 'package:app_xem_tro/screen/error_screen/error_screen.dart';
 import 'package:app_xem_tro/screen/forget_password_screen/forget_password_screen.dart';
 import 'package:app_xem_tro/screen/forget_password_screen/reset_password.dart';
 import 'package:app_xem_tro/screen/login_screen/login_screen.dart';
+import 'package:app_xem_tro/screen/profile_screen/detail_profile_screen.dart';
+import 'package:app_xem_tro/screen/profile_screen/profie_screen.dart';
 import 'package:app_xem_tro/screen/search_screen/search_screen.dart';
 import 'package:app_xem_tro/screen/signup_screen/second_signup.dart';
 import 'package:app_xem_tro/screen/navigation_screen.dart/navigation_screen.dart';
-import 'package:app_xem_tro/screen/profile_screen/profile_screen.dart';
 import 'package:app_xem_tro/screen/save_screen/save_screen.dart';
 import 'package:app_xem_tro/screen/signup_screen/signup_screen.dart';
 import 'package:app_xem_tro/screen/admin_screen/admin_login_screen.dart';
@@ -75,6 +80,26 @@ class RouteManager {
       name: "/adminLoginRoute",
       page: () => const AdminLogin(),
     ),
+    GetPage(
+      name: "/detailProfileRoute",
+      page: () => const DetailProfileScreen(),
+    ),
+    GetPage(
+      name: "/adminRoute",
+      page: () => const AdminScreen(),
+    ),
+    GetPage(
+      name: "/adminAccountRoute",
+      page: () => const AdminAccountScreen(),
+    ),
+    GetPage(
+      name: "/adminBookingRoute",
+      page: () => const AdminBookingScreen(),
+    ),
+    GetPage(
+      name: "/adminApproveRoute",
+      page: () => const AdminApproveScreen(),
+    )
   ];
 
   static GetPage notFound =
