5d6792953705ab8707c20769e7a3f902d60209af
havya123 <haivo006@gmail.com>
2023-10-31 13:43:47 +0700

    a

--- lib/screen/review_screen/review_screen.dart
+++ lib/screen/review_screen/review_screen.dart
@@ -7,7 +7,7 @@ import 'package:flutter_rating_bar/flutter_rating_bar.dart';
 import 'package:transparent_image/transparent_image.dart';
 
 class ReviewScreen extends StatelessWidget {
-  const ReviewScreen({super.key});
+  const ReviewScreen({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
@@ -18,186 +18,166 @@ class ReviewScreen extends StatelessWidget {
             padding: const EdgeInsets.symmetric(horizontal: 15),
             child: Column(
               children: [
-                SizedBox(
-                  height: getHeight(context, height: 0.2),
-                  width: double.infinity,
-                  child: Row(
-                    children: [
-                      Expanded(
-                          flex: 2,
-                          child: SizedBox(
-                            height: getHeight(context, height: 0.15),
-                            child: FadeInImage.memoryNetwork(
-                              fit: BoxFit.cover,
-                              placeholder: kTransparentImage,
-                              image:
-                                  "https://img.freepik.com/free-photo/blue-house-with-blue-roof-sky-background_1340-25953.jpg",
-                              imageErrorBuilder: (context, error, stackTrace) {
-                                return const Image(
-                                    image: AssetImage(
-                                        "assets/images/home_img/home.png"));
-                              },
-                            ),
-                          )),
-                      Expanded(
-                        flex: 5,
-                        child: Padding(
-                          padding: const EdgeInsets.symmetric(horizontal: 10),
-                          child: Text(
-                            "Nha ABCCCCCCCCCCCCCCccc",
-                            style: largeTextStyle(context),
-                          ),
-                        ),
-                      )
-                    ],
-                  ),
-                ),
+                buildHeader(context),
                 spaceHeight(context, height: 0.02),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Expanded(
-                        flex: 3,
-                        child: Text(
-                          "Chất lượng phòng trọ",
-                          style: mediumTextStyle(context),
-                        )),
-                    Expanded(
-                        flex: 5,
-                        child: RatingBar.builder(
-                          itemSize: 30,
-                          initialRating: 3,
-                          minRating: 1,
-                          direction: Axis.horizontal,
-                          allowHalfRating: true,
-                          itemCount: 5,
-                          itemPadding:
-                              const EdgeInsets.symmetric(horizontal: 4.0),
-                          itemBuilder: (context, _) => const Icon(
-                            Icons.star,
-                            color: Colors.amber,
-                          ),
-                          onRatingUpdate: (rating) {},
-                        )),
-                  ],
-                ),
+                buildRatingRow(context, "Chất lượng phòng trọ"),
                 spaceHeight(context, height: 0.01),
-                Row(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  mainAxisAlignment: MainAxisAlignment.start,
-                  children: [
-                    Expanded(
-                        flex: 3,
-                        child: Text(
-                          "Dịch vụ của người cho thuê",
-                          style: mediumTextStyle(context),
-                        )),
-                    Expanded(
-                        flex: 5,
-                        child: RatingBar.builder(
-                          itemSize: 30,
-                          initialRating: 3,
-                          minRating: 1,
-                          direction: Axis.horizontal,
-                          allowHalfRating: true,
-                          itemCount: 5,
-                          itemPadding:
-                              const EdgeInsets.symmetric(horizontal: 4.0),
-                          itemBuilder: (context, _) => const Icon(
-                            Icons.star,
-                            color: Colors.amber,
-                          ),
-                          onRatingUpdate: (rating) {},
-                        )),
-                  ],
-                ),
+                buildRatingRow(context, "Dịch vụ của người cho thuê"),
                 spaceHeight(context),
                 const Divider(
                   color: Colors.grey,
                   thickness: 0.5,
                 ),
                 spaceHeight(context),
-                Container(
-                  width: double.infinity,
-                  height: getHeight(context, height: 0.5),
-                  color: Colors.grey.shade300,
-                  child: const Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Padding(
-                        padding:
-                            EdgeInsets.symmetric(horizontal: 10, vertical: 10),
-                        child: Expanded(
-                            flex: 1,
-                            child: Text("Hãy để lại đánh giá của bạn!")),
-                      ),
-                      Divider(
-                        thickness: 1,
-                        color: Colors.grey,
-                      ),
-                      Expanded(
-                        flex: 9,
-                        child: TextFieldWidget(
-                          hint: "",
-                          removeBorder: true,
-                        ),
-                      ),
-                    ],
-                  ),
-                ),
+                buildReviewTextField(context),
                 spaceHeight(context),
-                ButtonWidget(
-                  function: () {
-                    showDialog<String>(
-                        context: context,
-                        builder: (BuildContext context) => AlertDialog(
-                              title: const Text('Xác nhận gửi đánh giá?'),
-                              content: const Text(
-                                  'Bạn có muốn gửi đánh giá này không?'),
-                              actions: <Widget>[
-                                TextButton(
-                                  onPressed: () =>
-                                      Navigator.pop(context, 'Cancel'),
-                                  child: const Text('Cancel'),
-                                ),
-                                TextButton(
-                                  onPressed: () {
-                                    const CircularProgressIndicator();
-                                    Future.delayed(const Duration(seconds: 1),
-                                        () {
-                                      showDialog(
-                                        context: context,
-                                        builder: (context) => AlertDialog(
-                                          title: const Text(
-                                              "Cảm ơn bạn đã gửi đánh giá"),
-                                          actions: [
-                                            TextButton(
-                                                onPressed: () {
-                                                  Navigator
-                                                      .pushReplacementNamed(
-                                                          context,
-                                                          Routes.homeRoute);
-                                                },
-                                                child: const Text("OK"))
-                                          ],
-                                        ),
-                                      );
-                                    });
-                                  },
-                                  child: const Text('OK'),
-                                ),
-                              ],
-                            ));
-                  },
-                  textButton: "Gửi đánh giá",
-                ),
-                spaceHeight(context)
+                buildSubmitButton(context),
+                spaceHeight(context),
               ],
             ),
           ),
         ),
       ),
+    );
+  }
+
+  Widget buildHeader(BuildContext context) {
+    return SizedBox(
+      height: getHeight(context, height: 0.2),
+      width: double.infinity,
+      child: Row(
+        children: [
+          Expanded(
+            flex: 2,
+            child: SizedBox(
+              height: getHeight(context, height: 0.15),
+              child: FadeInImage.memoryNetwork(
+                fit: BoxFit.cover,
+                placeholder: kTransparentImage,
+                image:
+                    "https://img.freepik.com/free-photo/blue-house-with-blue-roof-sky-background_1340-25953.jpg",
+                imageErrorBuilder: (context, error, stackTrace) {
+                  return const Image(
+                      image: AssetImage("assets/images/home_img/home.png"));
+                },
+              ),
+            ),
+          ),
+          Expanded(
+            flex: 5,
+            child: Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 10),
+              child: Text(
+                "Nha ABCCCCCCCCCCCCCCccc",
+                style: largeTextStyle(context),
+              ),
+            ),
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildRatingRow(BuildContext context, String title) {
+    return Row(
+      crossAxisAlignment: CrossAxisAlignment.start,
+      mainAxisAlignment: MainAxisAlignment.start,
+      children: [
+        Expanded(
+          flex: 3,
+          child: Text(
+            title,
+            style: mediumTextStyle(context),
+          ),
+        ),
+        Expanded(
+          flex: 5,
+          child: RatingBar.builder(
+            itemSize: 30,
+            initialRating: 3,
+            minRating: 1,
+            direction: Axis.horizontal,
+            allowHalfRating: true,
+            itemCount: 5,
+            itemPadding: const EdgeInsets.symmetric(horizontal: 4.0),
+            itemBuilder: (context, _) => const Icon(
+              Icons.star,
+              color: Colors.amber,
+            ),
+            onRatingUpdate: (rating) {},
+          ),
+        ),
+      ],
+    );
+  }
+
+  Widget buildReviewTextField(BuildContext context) {
+    return Container(
+      width: double.infinity,
+      height: getHeight(context, height: 0.5),
+      color: Colors.grey.shade300,
+      child: const Column(
+        crossAxisAlignment: CrossAxisAlignment.start,
+        children: [
+          Padding(
+            padding: EdgeInsets.symmetric(horizontal: 10, vertical: 10),
+            child: Text("Hãy để lại đánh giá của bạn!"),
+          ),
+          Divider(
+            thickness: 1,
+            color: Colors.grey,
+          ),
+          Expanded(
+            flex: 9,
+            child: TextFieldWidget(
+              hint: "",
+              removeBorder: true,
+            ),
+          ),
+        ],
+      ),
+    );
+  }
+
+  Widget buildSubmitButton(BuildContext context) {
+    return ButtonWidget(
+      function: () {
+        showDialog<String>(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+            title: const Text('Xác nhận gửi đánh giá?'),
+            content: const Text('Bạn có muốn gửi đánh giá này không?'),
+            actions: <Widget>[
+              TextButton(
+                onPressed: () => Navigator.pop(context, 'Cancel'),
+                child: const Text('Cancel'),
+              ),
+              TextButton(
+                onPressed: () {
+                  // Show a loading indicator
+                  showDialog(
+                    context: context,
+                    builder: (context) => AlertDialog(
+                      title: const Text("Cảm ơn bạn đã gửi đánh giá"),
+                      actions: [
+                        TextButton(
+                          onPressed: () {
+                            Navigator.pushReplacementNamed(
+                                context, Routes.homeRoute);
+                          },
+                          child: const Text("OK"),
+                        ),
+                      ],
+                    ),
+                  );
+                },
+                child: const Text('OK'),
+              ),
+            ],
+          ),
+        );
+      },
+      textButton: "Gửi đánh giá",
     );
   }
 }
