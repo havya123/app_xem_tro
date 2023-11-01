5e975281c57ceae8978407a3270b790a22e8eae6
havya123 <haivo006@gmail.com>
2023-11-01 21:26:10 +0700

    commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,9 +1,10 @@
 import 'package:app_xem_tro/app_tro.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:app_xem_tro/firebase_options.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const AppXemTro());
 }
