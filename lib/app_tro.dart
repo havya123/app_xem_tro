54a420264e1ec779197997c4c444c6be2bb53874
duccuong145 <nguyenduccuong130@gmail.com>
2023-11-29 15:05:32 +0700

    .

--- lib/app_tro.dart
+++ lib/app_tro.dart
@@ -15,7 +15,7 @@ class _AppXemTroState extends State<AppXemTro> {
   Widget build(BuildContext context) {
     return GetMaterialApp(
       debugShowCheckedModeBanner: false,
-      initialRoute: Routes.forgetRoute,
+      initialRoute: Routes.loginRoute,
       getPages: RouteManager.routeManager,
       unknownRoute: RouteManager.notFound,
     );
